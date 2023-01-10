60e3b1bdd7dcfc081c7dffc633e83e52bcb58a6b
camehu <camehu@gmail.com>
2023-01-10 14:20:39 -0300

    up-10-01-23-git

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-/* eslint-disable jsx-a11y/alt-text */
+
 import React from 'react';
-//import NavBar from './components/header/NavBar';
+import NavBar from './components/header/NavBar';
+
 
 
 
 function App() {
   return (
     <div className="App"> 
-      <img src="../public/img/landing_page.jpg" alt=""  />
+     <NavBar/>
      
            
     </div>
--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -1,10 +1,24 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
+import './NavBar.module.css'
 
 
 const NavBar = () => {
   return (
 
-    <div >
+    
+      <div className="wrapper">
+         <header>
+            <nav>
+              <div className="menu">
+                  <ul>
+                     <li><a href="#">Projetos</a></li>
+                     <li><a href="#">Sing in</a></li>
+                     
+                  </ul>
+               </div>
+            </nav>
+         </header>
     
     </div>
     
