7c46151a124679c62888342eb7526d2ec9249ede
camehu <camehu@gmail.com>
2023-01-10 12:11:23 -0300

    up-10-01-2023

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
-import NavBar from './components/header/NavBar';
+//import NavBar from './components/header/NavBar';
 
 
 
 function App() {
   return (
-    <div className="App">      
-      <img src='img/landing_page.jpg' alt='...'/> 
-      <NavBar/>    
+    <div className="App"> 
+      <img src="img/landing_page.jpg" alt=""  />
+     
+           
     </div>
   );
 }
