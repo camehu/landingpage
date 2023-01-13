eb73e7bc05f971fcb16a01d80b98d9137ea41efe
camehu <camehu@gmail.com>
2023-01-13 16:13:40 -0300

    up-13-01-22

---
+++ src/companents/header/NavBar.js
@@ -0,0 +1,25 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+import './navBar.modele.css'
+
+export default function NavBar() {
+  return (
+    <div class="wrapper">
+         <header>
+            <nav>
+                         
+               <div class="menu">
+                  <ul>
+                     <li><a href="#">Projetos</a></li>
+                     <li><a href="#">Sign-in</a></li>
+                     
+                  </ul>
+                  <div class="d-flex"><span>"O progresso não é Linear"</span></div>
+               </div>
+            </nav>
+            
+         </header>
+        
+        </div>
+  )
+}
